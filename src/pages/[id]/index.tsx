b295030b6e16bc9d6ad19183856b46e62e8913b7
Deepak Singh <singhdeepak5219@gmail.com>
2023-01-22 15:29:26 +0530

    getserversideprops

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import axios from "axios"
+import { GetServerSideProps } from "next"
 
-const index = () => {
+
+const getHeroDetails = () => {
   return (
     <div>hello</div>
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+  const {id} = params;
+  console.log(id);
+  const res = await axios.get(`http://localhost:3000/api/hero/${id}`);
 
-export default index
+  console.log(res.data.hero)
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
+export default getHeroDetails;
